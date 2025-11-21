fc92047ba303a1a251c816ac177f3145891467f4
username <you@example.com>
2025-11-20 19:37:59 -0600

    Solution for question 20 - print student using function

---
+++ question20.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+struct Student {
+    char name[40];
+    int roll;
+    float marks;
+};
+
+void printStudent(struct Student s) {
+    printf("\n--- Student Information ---\n");
+    printf("Name  : %s\n", s.name);
+    printf("Roll  : %d\n", s.roll);
+    printf("Marks : %.2f\n", s.marks);
+}
+
+int main() {
+    struct Student s;
+
+    printf("Enter student name: ");
+    scanf("%s", s.name);
+
+    printf("Enter roll number: ");
+    scanf("%d", &s.roll);
+
+    printf("Enter marks: ");
+    scanf("%f", &s.marks);
+
+    printStudent(s); // calling function
+
+    return 0;
+}
+
